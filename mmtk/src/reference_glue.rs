ff30f367d625f90745120fb80b2b02ba81013bcd
Steve Blackburn <steve.blackburn@anu.edu.au>
2020-05-14 18:38:43 +1000

    Revert to old is_live

--- mmtk/src/reference_glue.rs
+++ mmtk/src/reference_glue.rs
@@ -48,7 +48,7 @@ impl ReferenceGlue<JikesRVM> for VMReferenceGlue {
          * If the reference is dead, we're done with it. Let it (and
          * possibly its referent) be garbage-collected.
          */
-        if !reference.is_live() {
+        if !trace.is_live(reference) {
             VMReferenceGlue::clear_referent(reference);                   // Too much paranoia ...
             if TRACE_UNREACHABLE { trace!(" UNREACHABLE reference: {:?}", reference); }
             if TRACE_DETAIL { trace!(" (unreachable)"); }
@@ -74,7 +74,7 @@ impl ReferenceGlue<JikesRVM> for VMReferenceGlue {
 
         if TRACE_DETAIL { trace!(" => {:?}", new_reference); }
 
-        if old_referent.is_live() {
+        if trace.is_live(old_referent) {
             if cfg!(feature = "debug") {
                 // FIXME
                 /*if (!DebugUtil.validRef(oldReferent)) {
@@ -99,7 +99,7 @@ impl ReferenceGlue<JikesRVM> for VMReferenceGlue {
                     DebugUtil.dumpRef(oldReferent);
                     VM.sysFail("Invalid reference");
                 }*/
-                debug_assert!(new_reference.is_live());
+                debug_assert!(trace.is_live(new_reference));
             }
 
             /*
