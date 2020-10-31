42628fef6fbc6b889cdd8e21316e528e7fd0712e
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2020-10-31 19:11:21 +1100

    [CI Debug] Log stop_all_mutators only

--- mmtk/src/collection.rs
+++ mmtk/src/collection.rs
@@ -19,20 +19,20 @@ pub struct VMCollection {}
 impl Collection<JikesRVM> for VMCollection {
     #[inline(always)]
     fn stop_all_mutators<E: ProcessEdgesWork<VM = JikesRVM>>(tls: OpaquePointer) {
-        info!("stop_all_mutators");
+        println!("stop_all_mutators");
         unsafe {
             jtoc_call!(BLOCK_ALL_MUTATORS_FOR_GC_METHOD_OFFSET, tls);
         }
-        info!("stop_all_mutators finish");
+        println!("stop_all_mutators finish");
     }
 
     #[inline(always)]
     fn resume_mutators(tls: OpaquePointer) {
-        info!("resume_mutators");
+        println!("resume_mutators");
         unsafe {
             jtoc_call!(UNBLOCK_ALL_MUTATORS_FOR_GC_METHOD_OFFSET, tls);
         }
-        info!("resume_mutators finish");
+        println!("resume_mutators finish");
     }
 
     #[inline(always)]
