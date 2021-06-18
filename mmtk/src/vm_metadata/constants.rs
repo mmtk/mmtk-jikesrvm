1b7038f63e8be16f408a8715f8a3262f439345de
Javad Amiri <javad.amiri@anu.edu.au>
2021-06-18 19:00:08 +1000

    fix style issue

--- mmtk/src/vm_metadata/constants.rs
+++ mmtk/src/vm_metadata/constants.rs
@@ -1,9 +1,6 @@
 use std::usize;
 
-use mmtk::util::metadata::side_metadata::{
-    metadata_address_range_size, metadata_bytes_per_chunk, SideMetadataSpec,
-    GLOBAL_SIDE_METADATA_BASE_ADDRESS,
-};
+use mmtk::util::metadata::side_metadata::{SideMetadataSpec, GLOBAL_SIDE_METADATA_BASE_ADDRESS};
 use mmtk::util::metadata::{HeaderMetadataSpec, MetadataSpec};
 
 use crate::java_header::AVAILABLE_BITS_OFFSET;
@@ -12,44 +9,6 @@ pub(crate) use mmtk::util::constants::{
 };
 
 const FORWARDING_BITS_OFFSET: isize = AVAILABLE_BITS_OFFSET << LOG_BITS_IN_BYTE;
-
-/// Return the space size of the given metadata spec.
-/// As mmtk-jikesrvm only supports 32-bits:
-///  - For global metadata, the space size is the whole address range size, and
-///  - For local metadata, the space size is the size of metadata space needed per chunk.
-///
-#[cfg(not(debug_assert))]
-const fn side_metadata_size(metadata_spec: MetadataSpec) -> usize {
-    match metadata_spec {
-        MetadataSpec::OnSide(s) => {
-            if s.is_global {
-                metadata_address_range_size(&s)
-            } else {
-                metadata_bytes_per_chunk(s.log_min_obj_size, s.log_num_of_bits)
-            }
-        }
-        // this match arm is unreachable, but for a const function unreachable is not allowed
-        _ => 0,
-    }
-}
-
-#[cfg(debug_assert)]
-fn side_metadata_size(metadata_spec: MetadataSpec) -> usize {
-    debug_assert!(metadata_spec.is_on_side());
-    match metadata_spec {
-        MetadataSpec::OnSide(s) => {
-            if s.is_global {
-                metadata_address_range_size(&s)
-            } else {
-                metadata_bytes_per_chunk(s.log_min_obj_size, s.log_num_of_bits)
-            }
-        }
-        // this match arm is unreachable, but for a const function unreachable is not allowed
-        _ => {
-            unreachable!()
-        },
-    }
-}
 
 pub(crate) const LOG_BITS_IN_U16: usize = 4;
 
