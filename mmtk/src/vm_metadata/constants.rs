37836d611a5aa698ddf0834d1312d61253210535
Javad Amiri <javad.amiri@anu.edu.au>
2021-06-18 18:37:19 +1000

    update to the latest metadata structure

--- mmtk/src/vm_metadata/constants.rs
+++ mmtk/src/vm_metadata/constants.rs
@@ -1,9 +1,10 @@
 use std::usize;
 
 use mmtk::util::metadata::side_metadata::{
-    metadata_address_range_size, metadata_bytes_per_chunk, GLOBAL_SIDE_METADATA_BASE_ADDRESS,
+    metadata_address_range_size, metadata_bytes_per_chunk, SideMetadataSpec,
+    GLOBAL_SIDE_METADATA_BASE_ADDRESS,
 };
-use mmtk::util::metadata::MetadataSpec;
+use mmtk::util::metadata::{HeaderMetadataSpec, MetadataSpec};
 
 use crate::java_header::AVAILABLE_BITS_OFFSET;
 pub(crate) use mmtk::util::constants::{
@@ -17,11 +18,36 @@ const FORWARDING_BITS_OFFSET: isize = AVAILABLE_BITS_OFFSET << LOG_BITS_IN_BYTE;
 ///  - For global metadata, the space size is the whole address range size, and
 ///  - For local metadata, the space size is the size of metadata space needed per chunk.
 ///
+#[cfg(not(debug_assert))]
 const fn side_metadata_size(metadata_spec: MetadataSpec) -> usize {
-    if metadata_spec.is_global {
-        metadata_address_range_size(&metadata_spec)
-    } else {
-        metadata_bytes_per_chunk(metadata_spec.log_min_obj_size, metadata_spec.num_of_bits)
+    match metadata_spec {
+        MetadataSpec::OnSide(s) => {
+            if s.is_global {
+                metadata_address_range_size(&s)
+            } else {
+                metadata_bytes_per_chunk(s.log_min_obj_size, s.log_num_of_bits)
+            }
+        }
+        // this match arm is unreachable, but for a const function unreachable is not allowed
+        _ => 0,
+    }
+}
+
+#[cfg(debug_assert)]
+fn side_metadata_size(metadata_spec: MetadataSpec) -> usize {
+    debug_assert!(metadata_spec.is_on_side());
+    match metadata_spec {
+        MetadataSpec::OnSide(s) => {
+            if s.is_global {
+                metadata_address_range_size(&s)
+            } else {
+                metadata_bytes_per_chunk(s.log_min_obj_size, s.log_num_of_bits)
+            }
+        }
+        // this match arm is unreachable, but for a const function unreachable is not allowed
+        _ => {
+            unreachable!()
+        },
     }
 }
 
@@ -31,13 +57,13 @@ pub(crate) const LOG_BITS_IN_U16: usize = 4;
 
 /// Global logging bit metadata spec
 /// 1 bit per object
-pub(crate) const LOGGING_SIDE_METADATA_SPEC: MetadataSpec = MetadataSpec {
-    is_side_metadata: true,
-    is_global: true,
-    offset: GLOBAL_SIDE_METADATA_BASE_ADDRESS.as_isize(),
-    num_of_bits: 1,
-    log_min_obj_size: LOG_MIN_OBJECT_SIZE as usize,
-};
+pub(crate) const LOGGING_SIDE_METADATA_SPEC: MetadataSpec =
+    MetadataSpec::OnSide(SideMetadataSpec {
+        is_global: true,
+        offset: GLOBAL_SIDE_METADATA_BASE_ADDRESS.as_usize(),
+        log_num_of_bits: 0,
+        log_min_obj_size: LOG_MIN_OBJECT_SIZE as usize,
+    });
 
 // Global MetadataSpecs - End
 
@@ -45,57 +71,41 @@ pub(crate) const LOGGING_SIDE_METADATA_SPEC: MetadataSpec = MetadataSpec {
 
 /// PolicySpecific forwarding pointer metadata spec
 /// 1 word per object
-pub(crate) const FORWARDING_POINTER_METADATA_SPEC: MetadataSpec = MetadataSpec {
-    is_side_metadata: false,
-    is_global: false,
-    offset: FORWARDING_BITS_OFFSET,
-    num_of_bits: 1 << LOG_BITS_IN_WORD,
-    log_min_obj_size: LOG_MIN_OBJECT_SIZE as usize,
-};
+pub(crate) const FORWARDING_POINTER_METADATA_SPEC: MetadataSpec =
+    MetadataSpec::InHeader(HeaderMetadataSpec {
+        bit_offset: FORWARDING_BITS_OFFSET,
+        num_of_bits: 1 << LOG_BITS_IN_WORD,
+    });
 
 /// PolicySpecific object forwarding status metadata spec
 /// 2 bits per object
-pub(crate) const FORWARDING_BITS_METADATA_SPEC: MetadataSpec = MetadataSpec {
-    is_side_metadata: false,
-    is_global: false,
-    offset: FORWARDING_BITS_OFFSET,
-    num_of_bits: 2,
-    log_min_obj_size: LOG_MIN_OBJECT_SIZE as usize,
-};
+pub(crate) const FORWARDING_BITS_METADATA_SPEC: MetadataSpec =
+    MetadataSpec::InHeader(HeaderMetadataSpec {
+        bit_offset: FORWARDING_BITS_OFFSET,
+        num_of_bits: 2,
+    });
 
 /// PolicySpecific mark bit metadata spec
 /// 1 bit per object
-pub(crate) const MARKING_METADATA_SPEC: MetadataSpec = MetadataSpec {
-    is_side_metadata: false,
-    is_global: false,
-    offset: FORWARDING_BITS_OFFSET,
+pub(crate) const MARKING_METADATA_SPEC: MetadataSpec = MetadataSpec::InHeader(HeaderMetadataSpec {
+    bit_offset: FORWARDING_BITS_OFFSET,
     num_of_bits: 1,
-    log_min_obj_size: LOG_MIN_OBJECT_SIZE as usize,
-};
+});
 
 /// PolicySpecific mark-and-nursery bits metadata spec
 /// 2-bits per object
-pub(crate) const LOS_METADATA_SPEC: MetadataSpec = MetadataSpec {
-    is_side_metadata: false,
-    is_global: false,
-    offset: FORWARDING_BITS_OFFSET,
+pub(crate) const LOS_METADATA_SPEC: MetadataSpec = MetadataSpec::InHeader(HeaderMetadataSpec {
+    bit_offset: FORWARDING_BITS_OFFSET,
     num_of_bits: 2,
-    log_min_obj_size: LOG_MIN_OBJECT_SIZE as usize,
-};
+});
 
 // TODO: This is not used now, but probably needs to be double checked before being used.
-pub(crate) const UNLOGGED_SIDE_METADATA_SPEC: MetadataSpec = MetadataSpec {
-    is_side_metadata: true,
-    is_global: false,
-    offset: 0,
-    num_of_bits: 1,
-    log_min_obj_size: LOG_MIN_OBJECT_SIZE as usize,
-};
+pub(crate) const UNLOGGED_SIDE_METADATA_SPEC: MetadataSpec =
+    MetadataSpec::OnSide(SideMetadataSpec {
+        is_global: false,
+        offset: 0,
+        log_num_of_bits: 0,
+        log_min_obj_size: LOG_MIN_OBJECT_SIZE as usize,
+    });
 
 // PolicySpecific MetadataSpecs - End
-
-pub(crate) const LAST_GLOBAL_SIDE_METADATA_OFFSET: usize =
-    GLOBAL_SIDE_METADATA_BASE_ADDRESS.as_usize() + side_metadata_size(LOGGING_SIDE_METADATA_SPEC);
-
-pub(crate) const LAST_LOCAL_SIDE_METADATA_OFFSET: usize =
-    UNLOGGED_SIDE_METADATA_SPEC.offset as usize + side_metadata_size(UNLOGGED_SIDE_METADATA_SPEC);
