9d990d49642106b26456cc7c40ba32bf9631b1a8
Javad Amiri <javad.amiri@anu.edu.au>
2021-06-27 10:29:56 +1000

    update to the latest metadata

--- mmtk/src/vm_metadata/constants.rs
+++ mmtk/src/vm_metadata/constants.rs
@@ -1,6 +1,6 @@
 use std::usize;
 
-use mmtk::util::metadata::side_metadata::{SideMetadataSpec, GLOBAL_SIDE_METADATA_BASE_ADDRESS};
+use mmtk::util::metadata::side_metadata::{SideMetadataSpec, GLOBAL_SIDE_METADATA_VM_BASE_ADDRESS};
 use mmtk::util::metadata::{header_metadata::HeaderMetadataSpec, MetadataSpec};
 
 use crate::java_header::AVAILABLE_BITS_OFFSET;
@@ -15,7 +15,7 @@ const FORWARDING_BITS_OFFSET: isize = AVAILABLE_BITS_OFFSET << LOG_BITS_IN_BYTE;
 pub(crate) const LOGGING_SIDE_METADATA_SPEC: MetadataSpec =
     MetadataSpec::OnSide(SideMetadataSpec {
         is_global: true,
-        offset: GLOBAL_SIDE_METADATA_BASE_ADDRESS.as_usize(),
+        offset: GLOBAL_SIDE_METADATA_VM_BASE_ADDRESS.as_usize(),
         log_num_of_bits: 0,
         log_min_obj_size: LOG_MIN_OBJECT_SIZE as usize,
     });
