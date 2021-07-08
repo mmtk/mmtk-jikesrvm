95b6209609933cd115dee1349f25c8b0d5a1804b
Yi Lin <qinsoon@gmail.com>
2021-07-09 11:11:19 +1200

    Object model specs refactoring (#74)

--- mmtk/src/vm_metadata/constants.rs
+++ mmtk/src/vm_metadata/constants.rs
@@ -1,10 +1,8 @@
-use std::usize;
-
-use mmtk::util::metadata::side_metadata::{SideMetadataSpec, GLOBAL_SIDE_METADATA_VM_BASE_ADDRESS};
-use mmtk::util::metadata::{header_metadata::HeaderMetadataSpec, MetadataSpec};
+use mmtk::util::metadata::side_metadata::GLOBAL_SIDE_METADATA_VM_BASE_ADDRESS;
+use mmtk::vm::*;
 
 use crate::java_header::AVAILABLE_BITS_OFFSET;
-pub(crate) use mmtk::util::constants::{LOG_BITS_IN_BYTE, LOG_BITS_IN_WORD, LOG_MIN_OBJECT_SIZE};
+pub(crate) use mmtk::util::constants::LOG_BITS_IN_BYTE;
 
 const FORWARDING_BITS_OFFSET: isize = AVAILABLE_BITS_OFFSET << LOG_BITS_IN_BYTE;
 
@@ -12,13 +10,8 @@ const FORWARDING_BITS_OFFSET: isize = AVAILABLE_BITS_OFFSET << LOG_BITS_IN_BYTE;
 
 /// Global logging bit metadata spec
 /// 1 bit per object
-pub(crate) const LOGGING_SIDE_METADATA_SPEC: MetadataSpec =
-    MetadataSpec::OnSide(SideMetadataSpec {
-        is_global: true,
-        offset: GLOBAL_SIDE_METADATA_VM_BASE_ADDRESS.as_usize(),
-        log_num_of_bits: 0,
-        log_min_obj_size: LOG_MIN_OBJECT_SIZE as usize,
-    });
+pub(crate) const LOGGING_SIDE_METADATA_SPEC: VMGlobalLogBitSpec =
+    VMGlobalLogBitSpec::side(GLOBAL_SIDE_METADATA_VM_BASE_ADDRESS.as_usize());
 
 // Global MetadataSpecs - End
 
@@ -26,32 +19,22 @@ pub(crate) const LOGGING_SIDE_METADATA_SPEC: MetadataSpec =
 
 /// PolicySpecific forwarding pointer metadata spec
 /// 1 word per object
-pub(crate) const FORWARDING_POINTER_METADATA_SPEC: MetadataSpec =
-    MetadataSpec::InHeader(HeaderMetadataSpec {
-        bit_offset: FORWARDING_BITS_OFFSET,
-        num_of_bits: 1 << LOG_BITS_IN_WORD,
-    });
+pub(crate) const FORWARDING_POINTER_METADATA_SPEC: VMLocalForwardingPointerSpec =
+    VMLocalForwardingPointerSpec::in_header(FORWARDING_BITS_OFFSET);
 
 /// PolicySpecific object forwarding status metadata spec
 /// 2 bits per object
-pub(crate) const FORWARDING_BITS_METADATA_SPEC: MetadataSpec =
-    MetadataSpec::InHeader(HeaderMetadataSpec {
-        bit_offset: FORWARDING_BITS_OFFSET,
-        num_of_bits: 2,
-    });
+pub(crate) const FORWARDING_BITS_METADATA_SPEC: VMLocalForwardingBitsSpec =
+    VMLocalForwardingBitsSpec::in_header(FORWARDING_BITS_OFFSET);
 
 /// PolicySpecific mark bit metadata spec
 /// 1 bit per object
-pub(crate) const MARKING_METADATA_SPEC: MetadataSpec = MetadataSpec::InHeader(HeaderMetadataSpec {
-    bit_offset: FORWARDING_BITS_OFFSET,
-    num_of_bits: 1,
-});
+pub(crate) const MARKING_METADATA_SPEC: VMLocalMarkBitSpec =
+    VMLocalMarkBitSpec::in_header(FORWARDING_BITS_OFFSET);
 
 /// PolicySpecific mark-and-nursery bits metadata spec
 /// 2-bits per object
-pub(crate) const LOS_METADATA_SPEC: MetadataSpec = MetadataSpec::InHeader(HeaderMetadataSpec {
-    bit_offset: FORWARDING_BITS_OFFSET,
-    num_of_bits: 2,
-});
+pub(crate) const LOS_METADATA_SPEC: VMLocalLOSMarkNurserySpec =
+    VMLocalLOSMarkNurserySpec::in_header(FORWARDING_BITS_OFFSET);
 
 // PolicySpecific MetadataSpecs - End
