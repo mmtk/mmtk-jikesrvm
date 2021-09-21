a7c66c768d5b71b0214230e911d3e749ad758cae
Yi Lin <qinsoon@gmail.com>
2021-09-21 16:40:05 +1000

    Fix style

--- mmtk/src/collection.rs
+++ mmtk/src/collection.rs
@@ -52,7 +52,12 @@ impl Collection<JikesRVM> for VMCollection {
         _m: &T,
     ) {
         unsafe {
-            jtoc_call!(PREPARE_MUTATOR_METHOD_OFFSET, std::mem::transmute::<VMWorkerThread, usize>(tls_worker), std::mem::transmute::<VMMutatorThread, usize>(tls_mutator));
+            jtoc_call!(
+                PREPARE_MUTATOR_METHOD_OFFSET,
+                // convert to primitive types, so they can be used in asm!
+                std::mem::transmute::<_, usize>(tls_worker),
+                std::mem::transmute::<_, usize>(tls_mutator)
+            );
         }
     }
 
