9054a3e6d957c1e89ee6068d57127c67f4df2755
Yi Lin <qinsoon@gmail.com>
2022-11-16 10:40:35 +1100

    Print rust backtrace in our tests

--- .github/scripts/ci-test-assertions.sh
+++ .github/scripts/ci-test-assertions.sh
@@ -5,6 +5,8 @@ set -xe
 # To JikesRVM folder
 cd $JIKESRVM_PATH
 
+export RUST_BACKTRACE=1
+
 # RBaseBaseSemiSpaceAssertions
 ./bin/buildit localhost RBaseBaseSemiSpaceAssertions -j $JAVA_HOME --answer-yes --use-third-party-heap=$BINDING_PATH --use-third-party-build-configs=$BINDING_PATH/jikesrvm/build/configs --use-external-source=$BINDING_PATH/jikesrvm/rvm/src --m32
 ./dist/RBaseBaseSemiSpaceAssertions_x86_64_m32-linux/rvm $RVM_OPTIONS -Xms75M -Xmx75M -jar $DACAPO_PATH/dacapo-2006-10-MR2.jar fop
