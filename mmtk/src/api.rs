7b2f2e31e79f5dfe31a6ad7dab0af72bf3a0883e
Yi Lin <qinsoon@gmail.com>
2023-10-19 20:33:50 +1300

    Update api.rs

--- mmtk/src/api.rs
+++ mmtk/src/api.rs
@@ -197,7 +197,7 @@ pub extern "C" fn is_mapped_address(address: Address) -> i32 {
 }
 
 #[no_mangle]
-pub extern "C" fn modify_check(object: ObjectReference) {
+pub extern "C" fn modify_check(_object: ObjectReference) {
     // MMTk core no longe provides this method. We just use an empty impl.    
 }
 
