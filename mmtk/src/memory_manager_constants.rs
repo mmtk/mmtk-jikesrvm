fec74f08df227439033fd5ec631cde783de44ff4
Yi Lin <qinsoon@gmail.com>
2023-11-23 21:03:16 +0800

    Update to mmtk-core PR #1028 (#155)
    * Update to https://github.com/mmtk/mmtk-core/pull/1028
    * Move `num_specialized_scans` to the binding.
    
    ---------
    
    Co-authored-by: mmtkgc-bot <mmtkgc.bot@gmail.com>

--- mmtk/src/memory_manager_constants.rs
+++ mmtk/src/memory_manager_constants.rs
@@ -3,17 +3,21 @@ use crate::SELECTED_CONSTRAINTS;
 
 /** {@code true} if the selected plan needs support for linearly scanning the heap */
 pub const NEEDS_LINEAR_SCAN: bool = SELECTED_CONSTRAINTS.needs_linear_scan;
-/** Number of bits in the GC header required by the selected plan */
-pub const GC_HEADER_BITS: usize = SELECTED_CONSTRAINTS.gc_header_bits;
-/** Number of additional bytes required in the header by the selected plan */
-pub const GC_HEADER_BYTES: usize = SELECTED_CONSTRAINTS.gc_header_words << LOG_BYTES_IN_WORD;
 /** {@code true} if the selected plan requires concurrent worker threads */
 pub const NEEDS_CONCURRENT_WORKERS: bool = SELECTED_CONSTRAINTS.needs_concurrent_workers;
 /** {@code true} if the selected plan needs support for generating a GC trace */
-pub const GENERATE_GC_TRACE: bool = SELECTED_CONSTRAINTS.generate_gc_trace;
+pub const GENERATE_GC_TRACE: bool = false;
 /** {@code true} if the selected plan may move objects */
 pub const MOVES_OBJECTS: bool = SELECTED_CONSTRAINTS.moves_objects;
 /** {@code true} if the selected plan moves TIB objects */
 pub const MOVES_TIBS: bool = false;
 /** {@code true} if the selected plan moves code */
 pub const MOVES_CODE: bool = false;
+
+#[cfg(feature = "nogc")]
+pub const NUM_SPECIALIZED_SCANS: usize = 0;
+#[cfg(any(feature = "semispace", feature = "marksweep"))]
+pub const NUM_SPECIALIZED_SCANS: usize = 1;
+
+pub const GC_HEADER_BITS: usize = 2;
+pub const GC_HEADER_BYTES: usize = 0 << LOG_BYTES_IN_WORD;
