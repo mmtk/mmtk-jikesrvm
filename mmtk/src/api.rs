19692018e1a37472dabbfbc4a0e96300bc2e8cdf
Kunshan Wang <wks1986@gmail.com>
2024-04-24 15:05:18 +0800

    Use NullableObjectReference from mmtk-core

--- mmtk/src/api.rs
+++ mmtk/src/api.rs
@@ -5,6 +5,7 @@ use libc::c_char;
 use libc::c_void;
 use mmtk::memory_manager;
 use mmtk::scheduler::*;
+use mmtk::util::apiutils::NullableObjectReference;
 use mmtk::util::opaque_pointer::*;
 use mmtk::util::{Address, ObjectReference};
 
@@ -19,28 +20,6 @@ use JikesRVM;
 use BUILDER;
 use JTOC_BASE;
 use SINGLETON;
-
-/// An `Option<ObjectReference>` encoded as a `usize`.  It guarantees that `None` is encoded as 0,
-/// and `Some(objref)` is encoded as the underlying `usize` of `objref` itself.  It is intended for
-/// passing values to or from C++ code.
-/// Notes: The Rust ABI currently doesn't guarantee the encoding of `None`.
-#[repr(transparent)]
-pub struct NullableObjectReference(usize);
-
-impl From<NullableObjectReference> for Option<ObjectReference> {
-    fn from(value: NullableObjectReference) -> Self {
-        ObjectReference::from_raw_address(unsafe { Address::from_usize(value.0) })
-    }
-}
-
-impl From<Option<ObjectReference>> for NullableObjectReference {
-    fn from(value: Option<ObjectReference>) -> Self {
-        let encoded = value
-            .map(|obj| obj.to_raw_address().as_usize())
-            .unwrap_or(0);
-        Self(encoded)
-    }
-}
 
 /// # Safety
 /// Caller needs to make sure the ptr is a valid vector pointer.
